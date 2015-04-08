2d8d4f5434f1237517b7ebd00ffa74eb2e185617
Dustin Parham <dr.parham@gmail.com>
2015-04-08 16:57:35 -0600

    adding skip field variable to trait
    Need to be able to customize which fields you want to skip.

--- src/Traits/ModelSchemaBuilderTrait.php
+++ src/Traits/ModelSchemaBuilderTrait.php
@@ -3,6 +3,7 @@
 
 Trait ModelSchemaBuilderTrait {
 
+    protected $skip = array('created_at', 'deleted_at', 'active', '');
 
     public function getSchema(){
         return \DB::select(\DB::raw("DESCRIBE ".$this->table));
