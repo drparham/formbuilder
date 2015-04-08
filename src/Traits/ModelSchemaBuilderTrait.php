70c9d51478ac4d62d144328bceaa53c6e9bbba88
Dustin Parham <dr.parham@gmail.com>
2015-04-08 16:58:11 -0600

    Updating $skip
    …

--- src/Traits/ModelSchemaBuilderTrait.php
+++ src/Traits/ModelSchemaBuilderTrait.php
@@ -3,7 +3,7 @@
 
 Trait ModelSchemaBuilderTrait {
 
-    protected $skip = array('created_at', 'deleted_at', 'active', '');
+    protected $skip = array('created_at', 'deleted_at', 'active', 'updated_at');
 
     public function getSchema(){
         return \DB::select(\DB::raw("DESCRIBE ".$this->table));
