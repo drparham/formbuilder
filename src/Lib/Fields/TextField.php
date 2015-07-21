cb1bbb7b05b0191a0a9424bbd29251b267afee20
Dustin Parham <dr.parham@gmail.com>
2015-07-21 11:28:56 -0600

    Added TextField Class
    Hoping to populate text fields proeprly instead of defaulting to input fields.
    
    Signed-off-by: Dustin Parham <dr.parham@gmail.com>

---
+++ src/Lib/Fields/TextField.php
@@ -0,0 +1,24 @@
+<?php namespace Pta\Formbuilder\Lib\Fields;
+
+/**
+ * Class InputField implements Field Interface and Formats an Input Form Field
+ * @package Pta\Formbuilder\Lib\Fields
+ */
+class TextField implements FieldInterface{
+
+    /**
+     * Returns a Properly formatted Partial View of an Input Field
+     * @param $field
+     * @param $labels
+     * @param $
+     * @param $required
+     * @return Illuminate\View\View
+     */
+    public function getFormat($field, $labels, $fieldData = null, $required = false)
+    {
+        if(!is_null($fieldData)){
+            return view('pta/formbuilder::partials/fields/text')->with('field',$field->Field)->with('labels', $labels)->with('fieldData',$fieldData)->with('required',$required)->render();
+        }
+        return view('pta/formbuilder::partials/fields/text')->with('field',$field->Field)->with('labels', $labels)->with('required',$required)->render();
+    }
+}
--- src/Models/User.php
+++ src/Models/User.php
@@ -1,17 +1,21 @@
 <?php namespace Pta\Formbuilder\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Core\Model\User as Eloquent;
 use Pta\Formbuilder\Traits\ModelSchemaBuilderTrait;
 use Pta\Formbuilder\Lib\Fields\SelectField;
 
-class User extends Model
+class User extends Eloquent
 {
 
     use ModelSchemaBuilderTrait;
 
-    protected $table = 'users';
+    protected $table = 'westcott2.users';
 
     protected $skipFields = array('created_at', 'deleted_at', 'active', 'updated_at','permissions', 'last_login', 'password_date','remember_token','customer_id');
 
+//    public function school_id()
+//    {
+//        return new SelectField(new School, 'id', 'name');
+//    }
 
 }
