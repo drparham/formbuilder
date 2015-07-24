22176eb2c6e5b2ca8c18cf91d914fff7f46eed5e
Dustin Parham <dr.parham@gmail.com>
2015-07-24 12:48:01 -0600

    Allow to pass closure to SelectField
    Populate data for dropdown via closure
    
    Signed-off-by: Dustin Parham <dr.parham@gmail.com>

--- src/Lib/Fields/SelectField.php
+++ src/Lib/Fields/SelectField.php
@@ -30,25 +30,15 @@ class SelectField implements FieldInterface
     /**
      * Construct assigns required values
      * @param Model $model
-     * @param $id
      * @param $name
+     * @param $getData Callback
      */
-    public function __construct(Model $model, $id, $name)
+    public function __construct(Model $model, $name,  $getData)
     {
         $this->model = $model;
-        $this->id = $id;
-        $this->name = $name;
-    }
 
-    /**
-     * GetData pulls active data from the Model for the two columns only
-     * @return mixed
-     */
-    private function getData(){
-        if(is_subclass_of($this->model, 'Illuminate\Database\Eloquent\Model')){
-            return $this->model->where('active',1)->get(array($this->id,$this->name));
-        }
-        return false;
+        $this->name = $name;
+        $this->getData = $getData;
     }
 
     /**
@@ -62,12 +52,16 @@ class SelectField implements FieldInterface
      */
     public function getFormat($field, $labels, $fieldData = null, $required = false, $trans = null)
     {
-        $data = $this->getData();
+        $data = $this->getData;
+        $data = $data();
+
+
+        //dd($data);
 
         if(!is_null($fieldData)){
-            return view('pta/formbuilder::partials/fields/select')->with('data',$data)->with('field',$field->Field)->with('labels', $labels)->with('fieldData',$fieldData)->with('required',$required)->with('trans', $trans)->render();
+            return view('pta/formbuilder::partials/fields/select')->with('data',$data)->with('name',$this->name)->with('field',$field->Field)->with('labels', $labels)->with('fieldData',$fieldData)->with('required',$required)->with('trans', $trans)->render();
         }
-        return view('pta/formbuilder::partials/fields/select')->with('data',$data)->with('field',$field->Field)->with('labels', $labels)->with('required',$required)->with('trans', $trans)->render();
+        return view('pta/formbuilder::partials/fields/select')->with('data',$data)->with('name',$this->name)->with('field',$field->Field)->with('labels', $labels)->with('required',$required)->with('trans', $trans)->render();
 
     }
 }
