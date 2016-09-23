c850267fd006286ca03faeb9285e498684716f25
Dustin Parham <dr.parham@gmail.com>
2016-09-23 15:26:41 -0600

    New Model SChema builder class
    Models now extend this instead of using a trait. Allows us to pull config variables in a construct.

---
+++ src/Lib/ModelSchemaBuilder.php
@@ -0,0 +1,30 @@
+<?php namespace Pta\Formbuilder\Lib;
+
+use Illuminate\Database\Eloquent\Model;
+use Pta\Formbuilder\Lib\Fields\StateSelectField;
+use Pta\Formbuilder\Lib\Fields\CountrySelectField;
+use Pta\Formbuilder\Traits\ModelSchemaBuilderTrait;
+
+class ModelSchemaBuilder extends Model
+{
+    use ModelSchemaBuilderTrait;
+
+    protected $defaultFields = [];
+
+    protected $defaultInputs = [];
+
+    protected $defaultLabels = [];
+
+    protected $protected = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->defaultFields = \Config('formbuilder.fields');
+        $this->defaultInputs = \Config('formbuilder.inputs');
+        $this->defaultLabels = \Config('formbuilder.labels');
+        $this->protected = \Config('formbuilder.protected');
+
+    }
+
+}
