216eeca9112228b0d078d48c182c7609d0ea4abf
Dustin Parham <dr.parham@gmail.com>
2016-09-30 17:55:22 -0600

    added [] to end of name to capture array of products

--- resources/view/partials/fields/multi-select.blade.php
+++ resources/view/partials/fields/multi-select.blade.php
@@ -2,7 +2,7 @@
     <div class="form-group @if($errors->has($field) ) has-error @endif">
         <label for="<?php echo $field; ?>" class="col-md-3 control-label"><?php if(isset($labels[$field])){ echo $labels[$field]; }else { echo $field; } ?></label>
         <div class="col-md-9">
-            <select id="{{ $field }}" name="{{ $field }}" <?php if($required=="NO") echo 'required';?> class="form-control select" multiple>
+            <select id="{{ $field }}" name="{{ $field }}[]" <?php if($required=="NO") echo 'required';?> class="form-control select" multiple>
                 @foreach($data as $option)
                     @if(isset($fieldData))
                         @if($option->id == $fieldData)
@@ -22,7 +22,7 @@
     <div class="form-group @if($errors->has($field) ) has-error @endif">
         <label for="<?php echo $field; ?>" class="col-md-3 control-label">{{ trans($trans.'model.general.'.$field) }}</label>
         <div class="col-md-9">
-            <select id="{{ $field }}" name="{{ $field }}" <?php if($required=="NO") echo 'required';?> class="form-control select" multiple>
+            <select id="{{ $field }}" name="{{ $field }}[]" <?php if($required=="NO") echo 'required';?> class="form-control select" multiple>
                 @foreach($data as $option)
                     @if(isset($fieldData))
                         @if($option->id == $fieldData)
